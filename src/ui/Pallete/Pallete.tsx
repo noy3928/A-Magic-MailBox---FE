0bd7c947ce1e2e54e69cd4f6d9a068837e030866
yechan <noy3928@naver.com>
2022-05-01 16:29:06 +0900

    feat: add pallete file

---
+++ src/ui/Pallete/Pallete.tsx
@@ -0,0 +1,17 @@
+import react from "React";
+import styled from "@emotion/styled";
+import { palletes } from "./palletes";
+
+const Pallete = () => {
+  return <Container></Container>;
+};
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: #f4f4f4;
+`;
+
+const Circle = styled.div``;
+
+export default Pallete;
