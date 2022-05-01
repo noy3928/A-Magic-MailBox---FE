adb8607d390bc766f21557db674eff825f51dfce
yechan <noy3928@naver.com>
2022-05-01 16:41:54 +0900

    feat: make circle in pallete component

--- src/ui/Pallete/Pallete.tsx
+++ src/ui/Pallete/Pallete.tsx
@@ -2,16 +2,32 @@ import react from "React";
 import styled from "@emotion/styled";
 import { palletes } from "./palletes";
 
+type StyleTheme = {
+  bgColor: string;
+};
+
 const Pallete = () => {
-  return <Container></Container>;
+  return (
+    <Container>
+      {palletes.map((data) => {
+        return <Circle bgColor={data.bgColor} />;
+      })}
+    </Container>
+  );
 };
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   background-color: #f4f4f4;
+  padding: 20px;
 `;
 
-const Circle = styled.div``;
+const Circle = styled.div<StyleTheme>`
+  width: 50px;
+  height: 50px;
+  border-radius: 25px;
+  background-color: ${(props) => props.bgColor};
+`;
 
 export default Pallete;
